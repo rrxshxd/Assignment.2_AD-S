16691bccb4dfda9cfdaf03eb7e8894fbf86a903d
Rashad <230924@astanait.edu.kz>
2024-04-24 17:25:24 +0600

    Finished MyArrayList without sort()

--- src/Classes/MyArrayList.java
+++ src/Classes/MyArrayList.java
@@ -3,10 +3,11 @@ package Classes;
 import Interfaces.MyList;
 
 public class MyArrayList<T> implements MyList<T> {
+    private int capacity = 10;
     private T[] arr;
     private int size;
     public MyArrayList() {
-        arr = (T[]) new Object[5];
+        arr = (T[]) new Object[capacity];
         size = 0;
     }
 
@@ -19,7 +20,14 @@ public class MyArrayList<T> implements MyList<T> {
         arr[size++] = data;
     }
 
-    public void addElement(T element, int index) {
+    @Override
+    public void setElement(int index, T element) {
+        checkIndex(index);
+        arr[index] = element;
+    }
+
+    @Override
+    public void addElement(int index, T element) {
         if (size >= arr.length) {
             increaseBuffer();
         }
@@ -27,8 +35,19 @@ public class MyArrayList<T> implements MyList<T> {
         arr[index] = element;
     }
 
+    @Override
+    public void addFirst(T element) {
+        addElement(0,element);
+    }
+
+    @Override
+    public void addLast(T element) {
+        addElement(element);
+    }
+
     private void increaseBuffer() {
-        T[] newArr = (T[]) new Object[arr.length * 2];
+        capacity = capacity * 2;
+        T[] newArr = (T[]) new Object[capacity];
         for(int i = 0; i < size; i++) {
             newArr[i] = arr[i];
         }
@@ -39,6 +58,20 @@ public class MyArrayList<T> implements MyList<T> {
     public T getElement(int index) {
         checkIndex(index);
         return arr[index];
+    }
+
+    @Override
+    public T getFirst() {
+        if (size == 0)
+            System.out.println("List is empty");
+        return arr[0];
+    }
+
+    @Override
+    public T getLast() {
+        if (size == 0)
+            System.out.println("List is empty");
+        return arr[size-1];
     }
 
     private void checkIndex(int index) {
@@ -57,18 +90,70 @@ public class MyArrayList<T> implements MyList<T> {
         System.out.println();
     }
     @Override
-    public T remove(int index) {
+    public void remove(int index) {
         checkIndex(index);
-        T oldValue = arr[index];
-        for (int i = index; i < size - 1; i++) {
-            arr[i] = arr[i + 1];
+        for (int i = index+1; i < size; i++) {
+            arr[i-1] = arr[i];
         }
         size--;
-        return oldValue;
     }
+
+    @Override
+    public void removeFirst() {
+        if (size == 0)
+            System.out.println("List is empty");
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        if (size == 0)
+            System.out.println("List is empty");
+        remove(size-1);
+    }
+
+    @Override
+    public void sort() {
+        boolean swapped;
+        for (int i = 0; i < size - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] != null && arr[j + 1] != null && )
+            }
+        }
+    }
+
+    @Override
+    public T[] toArray() {
+        T[] array = (T[]) new Object[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = arr[i];
+        }
+        return array;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) >= 0;
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == object) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public void clear() {
-        arr = (T[]) new Object[5];
         size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
     }
 
 }
--- src/Interfaces/MyList.java
+++ src/Interfaces/MyList.java
@@ -2,6 +2,20 @@ package Interfaces;
 
 public interface MyList<T> {
     void addElement(T data);
+    void setElement(int index, T element);
+    void addElement(int index, T element);
+    void addFirst(T element);
+    void addLast(T element);
     T getElement(int index);
-    T remove(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int indexOf(Object object);
+    boolean exists(Object object);
+    public T[] toArray();
+    void clear();
+    int size();
 }
